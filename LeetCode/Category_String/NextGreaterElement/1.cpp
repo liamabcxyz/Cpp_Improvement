3d38ba65b9ee6172a1fd57aeba52b459115b85ae
liamabcxyz <70308500+liamabcxyz@users.noreply.github.com>
2021-08-09 07:33:42 -0700

    Create 1.cpp
    next_permutation()

---
+++ LeetCode/Category_String/NextGreaterElement/1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	//main function
+    int nextGreaterElement(int n) {
+		//convert n to string
+        string number = to_string(n);
+		
+		//check if next permutation exists
+        if(next_permutation(number.begin(),number.end())){
+			//if yes convert it to long number
+            long long num = stoll(number);
+			//return num if it's in range
+            if(num <= INT_MAX and num >= INT_MIN)
+                return num;
+        }
+		//return -1
+        return -1;
+    }
+};
