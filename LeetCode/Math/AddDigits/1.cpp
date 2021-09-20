472ee6c6f0f724d7df67fde3c86f402861393b76
liamabcxyz <70308500+liamabcxyz@users.noreply.github.com>
2021-09-20 15:19:22 -0700

    Create 1.cpp
    Runtime: 3 ms, faster than 42.91% of C++ online submissions for Add Digits.
    Memory Usage: 5.8 MB, less than 74.20% of C++ online submissions for Add Digits.

---
+++ LeetCode/Math/AddDigits/1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10)
+        {
+            return num;
+        }
+        else
+        {
+            int res=0;
+            while(num>0)
+            {
+                res+=num%10;
+                num=num/10;
+            }
+            return addDigits(res);
+        }
+    }
+};
