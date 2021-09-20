f25e17f27a67bc5f8ef158ffdca9f70a26d727fd
liamabcxyz <70308500+liamabcxyz@users.noreply.github.com>
2021-09-19 20:04:54 -0700

    Create 2.cpp
    Runtime: 128 ms, faster than 18.10% of C++ online submissions for Subarray Sum Equals K.
    Memory Usage: 41.6 MB, less than 26.17% of C++ online submissions for Subarray Sum Equals K.

---
+++ LeetCode/Math/SubarraySumEqualsK/2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int res=0;
+        int sum=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            res+=mp[sum-k];
+            mp[sum]++;
+        }
+        return res;
+    }
+};
