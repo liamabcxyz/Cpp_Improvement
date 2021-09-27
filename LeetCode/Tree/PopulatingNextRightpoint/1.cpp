fef52e60079e477b3f5567c4eb7d79f573724d8b
liamabcxyz <70308500+liamabcxyz@users.noreply.github.com>
2021-09-27 08:22:01 -0700

    Create 1.cpp
    Runtime: 8 ms, faster than 96.43% of C++ online submissions for Populating Next Right Pointers in Each Node II.
    Memory Usage: 17.5 MB, less than 43.46% of C++ online submissions for Populating Next Right Pointers in Each Node II.

---
+++ LeetCode/Tree/PopulatingNextRightpoint/1.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)
+            return NULL;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int size=q.size();
+            Node* tn;
+            
+            for(int i=0;i<size;i++)
+            {
+                tn = q.front();
+                q.pop();
+
+                if(tn->left)
+                    q.push(tn->left);
+                if(tn->right)
+                    q.push(tn->right);
+                
+                if(i<size-1)
+                    tn->next = q.front();
+            }
+        }
+        
+        return root;
+    }
+};
